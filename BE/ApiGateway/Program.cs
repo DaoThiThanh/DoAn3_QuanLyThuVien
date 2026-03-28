38a8566bccc65517105ea269676302553fe0a1f0
DaoThiThanh <thanhthanhdao178@gmail.com>
2026-03-28 16:53:42 +0700

    Cấu hình file ocelot

--- BE/ApiGateway/Program.cs
+++ BE/ApiGateway/Program.cs
@@ -1,27 +1,14 @@
-namespace ApiGateway
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+var builder = WebApplication.CreateBuilder(args);
+// yêu cầu hệ thống nạp cấu hình từ file ocelot.json
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
-            // Add services to the container.
+// Đăng ký dịch vụ Ocelot
+builder.Services.AddOcelot(builder.Configuration);
+var app = builder.Build();
+app.MapGet("/", () => "Hello from API Gateway!");
+// Sử dụng middleware của Ocelot 
+await app.UseOcelot();
 
-            builder.Services.AddControllers();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
-}
+app.Run();
